9c32e95b7765052689b5a938b324f57d0ca890e6
Grace F. Baumgardner <Grace-CB@users.noreply.github.com>
2014-11-26 20:00:07 -0500

    Incorporated previous structure notes

--- Octypus.rb
+++ Octypus.rb
@@ -1,8 +1,29 @@
+#!/usr/bin/env ruby
+
+$LOAD_PATH << '.'
+
+require_relative 'Tenticle'
+
+# Start up (add check-for-command-line-flags eventually but not right now)
+
+A = Tenticle::Arm.initialize(ARGV) # Should catch the status of initialization for error decomposition
+
+B = Tenticle::Arm.new() # This is going to throw our errors and suchness
+
+B.Arm.ize("Cupsize", "babycakes")
+
+B.Arm.err("You wanted to know, baby.", 2)
+B.Arm.err("Just a jerk.", 1)
+B.Arm.err("Calumny and perfidy!", 0)
+
+B.Arm.err("This doesn't happen.", 2)
+
+
+
 # This is where the Octypus primary logic goes
 # we'll write a helper module from the half-built version after we set up SVN for eclipse
 
-# Start up (add check-for-command-line-flags eventually but not right now)
-#
+
 # Load a config file (ideally in YAML so I don't have to reconceptualize that)
 #
 # Execute the given test the number of times indicated on the servers given
