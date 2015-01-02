0ec13e6dac8f85359a8451d6b764d231a6699412
Grace F. Baumgardner <Grace-CB@users.noreply.github.com>
2015-01-02 17:27:08 -0500

    * Fixed error handling issue * Added yaml grabbing

--- Octypus.rb
+++ Octypus.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require './Tenticle'
+require 'yaml'
 
 # Start up (add check-for-command-line-flags eventually but not right now)
 
@@ -8,15 +9,17 @@ a = Tenticle::Cups.new(ARGV)  # Create a tenticle and init default options,
                               # check for incoming command line options,
                               # etc. etc.
 
-puts a.list
+
+
+#File.open('database.yml', 'w') do |file|
+#  file.write(Psych.dump([b, c, d]))
+#end
 
 a.err("This is diagnostic level, for FYI messages.", 2)
 a.err("Warning level, for things that aren't fatal.", 1)
 a.err("Exception level! Perfidy!", 0)
-a.err("This doesn't happen.", 5)
+#a.err("This doesn't happen.", 5)
 
-
-#puts "We didn't quit, or else this wouldn't be here."
 
 # Load a config file (ideally in YAML so I don't have to reconceptualize that)
 #
@@ -29,3 +32,6 @@ a.err("This doesn't happen.", 5)
 # Shut down
 #
 
+#TODO: Concurrency? Can we create a 'matrix of events' style yaml structure in order to assign sequences and simultaneous tests?
+#TODO: Fuzzy filefinding? If we get a typo'd name for a test, can we look for the closest thing? Is that even a good idea?
+#INFO: I don't want to get into full scripting on this.
--- Tenticle.rb
+++ Tenticle.rb
@@ -7,16 +7,19 @@ module Tenticle
 
   class Cups
 
-      attr_accessor :file, :verbose, :errorlevel, :times, :list
+    # Cups provides Tenticle with grip. In this case, that means setting configuration
+    # state and handling command line alterations for it.
 
-#fixme: this has a non termination error
+    attr_accessor :file, :verbose, :errorlevel, :times, :list
 
     def initialize (args)
 
       # These define the basic configuration. They're altered by command line options.
 
-      @file = 'default.yaml'             # #TODO: Create a self-creator if default.yaml doesn't exist
-      @verbose = false                   # Keep it quiet
+      @tests = ['test1', 'test2', 'test3']
+      @servers = ['avanboxel', 'qa-eris']
+      @file = ''
+      @verbose = false                  # Keep it quiet
       @errorlevel = 2                   # Fatals
       @times = 3                        # By default, if you don't specify repetitions, there's just three.
 
@@ -26,19 +29,36 @@ module Tenticle
           err("Setting config filename as '#{ args[count+1] }'", 2)
           @file = args[count+1]
 
+# Fix this functionality later, so that tests and servers tested can be specified
+# on the command line.
+#
+#        elsif (arg == ('-s')) then
+#          err("Servers specified as '#{ (args[count+1]).join(', ') }", 2)
+#          @servers = args[count+1]
+#
+#        elsif (arg == ('-t')) then
+#          err("Tests specified as '#{ (args[count+1]).join(', ') } ", 2)
+#          @tests = args[count+1]
+
         elsif (arg == ('-v')) then
-          err("Setting verbose to true", 2)
+          err("Verbose on", 2)
           @verbose = true
 
         elsif (arg == ('-e')) then
           err("Setting errorlevel to #{ args[count+1] }", 2)
           @errorlevel = args[count+1]
 
-        elsif (arg == ('-t')) then
-          err("Setting times to #{ args[count+1] }", 2)
+        elsif (arg == ('-i')) then
+          err("Setting iterations (#{ @times }) to #{ args[count+1] }", 2)
           @times = args[count+1]
 
+        elsif (arg == args[-1]) then
+          err("Hit the last argument, so we're popping out.", 2)
+          next
+
         else
+
+          err("Unidentified command line flags used.", 0)
 
         end
 
@@ -79,14 +99,16 @@ module Tenticle
         exit
 
       elsif (level == 1) then
-        puts warnize(message)
+
+        if (@errorlevel >= 1) then puts warnize(message) end
 
       elsif (level == 2) then
-        puts diagnize(message)
+
+        if (@errorlevel >= 2) then puts diagnize(message) end
 
       else
 
-        puts "Somehow we fell off the thing. You got me, bub."
+        err("Error handling error ", 0)
 
       end
 
